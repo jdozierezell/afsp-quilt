85489a267fdf442cfda305d8bfa9ec1fafcf9d57
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-04-01 06:09:03 -0500

    updating quilt squares through april 1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,57 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
+const queries = require('./src/utils/algolia')
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
-  /* Your site config here */
+	siteMetadata: {
+		title: `Gatsby Default Starter`,
+		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+		author: `@gatsbyjs`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-playground`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		{
+			resolve: `gatsby-source-datocms`,
+			options: {
+				apiToken: process.env.DATOCMS_API,
+				previewMode: true,
+				disabledLiveReload: false,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-algolia`,
+			options: {
+				appId: process.env.GATSBY_ALGOLIA_APP_ID,
+				apiKey: process.env.ALGOLIA_ADMIN_KEY,
+				queries,
+				chunkSize: 10000, // default: 1000
+			},
+		},
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+		`gatsby-plugin-client-side-redirect`, // keep it in last in list
+	],
 }
